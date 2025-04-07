d78f7a4d443eea23130f3b73023c0b4c17bd690d
Raphael Meimoun <raphael.meimoun95@gmail.com>
2025-04-07 14:00:03 +0200

    init projet

---
+++ src/main/java/com/raphmei/userhub/UserhubApplication.java
@@ -0,0 +1,13 @@
+package com.raphmei.userhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raphmei/userhub/UserhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raphmei.userhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
